b717295a510d66fbc7ae5b47588ef7c27b3be050
GuiBDBello <guilhermedallbello@hotmail.com>
2024-07-09 10:03:28 -0300

    Remove maze generation scenes and scripts to start anew

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,18 +5,4 @@ extends Node
 
 
 func _ready():
-	var width = 5
-	var depth = 5
 	var is_generated_slowly = true
-	
-	get_maze_generator().generate(width, depth, is_generated_slowly)
-	center_maze_on_screen(width, depth)
-
-
-func center_maze_on_screen(maze_width, maze_depth):
-	get_maze_generator().position.x = 0 - (maze_width / 2)
-	get_maze_generator().position.z = 0 - (maze_depth / 2)
-
-
-func get_maze_generator():
-	return get_node("./Maze Generator")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,23 +1,14 @@
 extends Control
 
-const MAZE_GENERATOR = preload("res://scenes/maze_generator.tscn")
-
 var maze_width = 5
 var maze_height = 5
 var is_generated_slowly_toggled_on = false
 
 @onready var game = $".."
-@onready var maze_generator = $"../Maze Generator"
 
 
 func _on_generate_maze_pressed():
-	maze_generator.queue_free()
-	await maze_generator.tree_exited
-	maze_generator = MAZE_GENERATOR.instantiate()
-	maze_generator.name = "Maze Generator"
-	game.add_child(maze_generator)
-	maze_generator.generate(self.maze_width, self.maze_height, self.is_generated_slowly_toggled_on)
-	game.center_maze_on_screen(self.maze_width, self.maze_height)
+	pass
 
 
 func _on_h_slider_width_value_changed(value):
