29f641e079222a7b1a8cb7aa46065e8c6f1381d0
GuiBDBello <guilhermedallbello@hotmail.com>
2024-08-27 21:49:28 -0300

    Minor changes (really nothing important)

--- scenes/maze/maze_cell.gd
+++ scenes/maze/maze_cell.gd
@@ -11,13 +11,13 @@ var is_visited : bool = false
 
 
 func visit() -> void:
-	is_visited = true
+	self.is_visited = true
 
 
 func set_wall_sizes(wall_length: int, wall_height: int, wall_thickness: float) -> void:
-	set_wall_length(wall_length)
-	set_wall_height(wall_height)
-	set_wall_thickness(wall_thickness)
+	self.set_wall_length(wall_length)
+	self.set_wall_height(wall_height)
+	self.set_wall_thickness(wall_thickness)
 
 
 func set_wall_length(wall_length: int) -> void:
